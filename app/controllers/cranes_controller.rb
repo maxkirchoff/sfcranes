0d429405779a2322df6488aa9ec19d846b757980
Max Kirchoff <max@thebolditalic.com>
2014-04-27 13:12:04 -0700

    added all basic reporting functionality

--- app/controllers/cranes_controller.rb
+++ app/controllers/cranes_controller.rb
@@ -7,16 +7,27 @@ class CranesController < ApplicationController
     @crane = Crane.new(crane_params)
 
     @crane.save
-    redirect_to @crane
+    render :json => @crane
   end
 
   def show
     @crane = Crane.find(params[:id])
   end
 
+  def index
+    @cranes = Crane.all
+  end
+
+  def update
+    @crane = Crane.find(params[:id])
+    @crane.update(crane_params)
+    @crane.save
+    render :json => @crane
+  end
+
   private
     def crane_params
-      params.require(:crane).permit(:title, :notes)
+      params.require(:crane).permit(:lat, :lon, :title, :notes)
     end
 end
 
